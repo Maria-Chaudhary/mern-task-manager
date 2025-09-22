b9c3bf35465c2097ec860e3483021e5acd623394
Maria <mariachaudhary008@gmail.com>
2025-09-22 22:46:15 +0500

    Polish Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
